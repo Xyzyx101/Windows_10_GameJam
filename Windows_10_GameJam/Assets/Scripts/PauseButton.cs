562a491625217fec7c7bcf4464b5af7502fe654f
Shawn Sercombe <shawnsercombe@gmail.com>
2015-05-17 14:07:43 -0400

    Added main menu and interface

---
+++ Windows_10_GameJam/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseButton : MonoBehaviour
+{
+	public GameObject pauseButton, pausePanel;
+
+	public void Start()
+	{
+		OnResume();
+	}
+
+	public void OnPause()
+	{
+		pausePanel.SetActive(true);
+		pauseButton.SetActive(false);
+		Time.timeScale = 0;
+	}
+
+	public void OnResume()
+	{
+		pausePanel.SetActive(false);
+		pauseButton.SetActive(true);
+		Time.timeScale = 1;
+	}
+
+	public void OnRestart(string level)
+	{
+		pausePanel.SetActive(false);
+		pauseButton.SetActive(true);
+		Time.timeScale = 1;
+		Application.LoadLevel(level);
+		return;
+	}
+}
---
+++ Windows_10_GameJam/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour 
+{
+	public GameObject playButton;
+
+	public void Start()
+	{
+		playButton.SetActive(true);
+	}
+
+	public void OnPlay(string level)
+	{
+		Application.LoadLevel(level);
+		return;
+	}
+}
